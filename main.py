9a95ba3d4d736a5011d2b09b486f06c008820bca
Miguel Silva <a18829@alunos.ipca.pt>
2020-11-11 23:46:56 +0000

    v4

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-import re
+#import re
 import ply.lex as lex
-from utils import readFile, Test, printTests
-tests = []
-tokens = ("OK", "NOTOK", "SUBOK", "TAB", "COMMENT", "COUNTER")
+from utils import readFile, Test, getNivel
 
+tests = []
+
+# t1 = Test(1, True, "test 1", 1)
+# t2 = Test(2, True, "test sub1", 2)
+# t3 = Test(3, True, "test sub sub1", 3)
+# t4 = Test(4, True, "test 2", 1)
+# t2.subtests.append(t3)
+# t1.subtests.append(t2)
+# tests.append(t1)
+# tests.append(t4)
+#
+# for t in tests:
+#     t.printTests()
+
+tokens = ("OK", "NOTOK", "SUBOK", "SUBNOTOK", "TAB", "COMMENT", "COUNTER")
 
 def t_COUNTER(t):
     r"\d.+.\n"
@@ -13,13 +26,16 @@ def t_OK(t):
     r"^ok.+\n"
     return t
 
-
 def t_SUBOK(t):
-   r"(\s{4})+ok.+\n"
-   return t
+    r"(\s{4})+ok.+\n"
+    return t
 
 def t_NOTOK(t):
-    r"not\sok.+\n"
+    r"^not\sok.+\n"
+    return t
+
+def t_SUBNOTOK(t):
+    r"(\s{4})+not\sok.+\n"
     return t
 
 def t_TAB(t):
@@ -34,24 +50,20 @@ def t_error(t):
     print("Unknown token: [%s]" % t.value)
     exit(1)
 
+pointer_record = []
 lexer = lex.lex()
 lexer.input(readFile("test/teste3.t"))
 
 for token in iter(lexer.token, None):
+    pass
     if token.type == "SUBOK":
-        s=token.value
-        ws=len(s) - len(s.lstrip())
-        ws=ws/4
-        ws+=1
-        print("Nivel:%s" % ws)
-    # print(token)
-    # if token.type== "OK":
-    #     tests.append(Test(int(token.value[3]),True,token.value[6:]))
-    # if token.type== "NOTOK":
-    #     tests.append(Test(int(token.value[3]),False,token.value[6:]))
+        ws = getNivel(token.value)
+        saux = token.value.strip()
+        taux = Test(saux[3], True, saux[6:], ws)
+    if token.type == "OK":
+        taux = Test(int(token.value[3]), True, token.value[6:], 1)
+    if token.type == "NOTOK":
+        taux = Test(int(token.value[3]), False, token.value[6:], 1)
 
 # for test_ in tests:
-#     printTests(test_);
-
-
-
+#     test_.printTests()
--- utils.py
+++ utils.py
@@ -6,19 +6,40 @@ def readFile(filename):
     fh.close()
     return contents
 
+def getNivel(string):
+    ws = len(string) - len(string.lstrip())
+    ws=ws/4
+    ws+=1
+    return int(ws)
+
+
 
 class Test:
-    def __init__(self, id, ok, desc):
+    def __init__(self, id, ok, desc,nivel):
         self.id = id
         self.ok = ok
         self.desc = desc
+        self.nivel=nivel
         self.subtests=[]
 
-def printTests(t):
-    print("Id:%s" % t.id)
-    if t.ok == True:
-        print("Resultado:%s" % "PASSOU")
-    else:
-        print("Resultado:%s" % "NÃO PASSOU")
-    print("Description:%s" % t.desc)
+    def getPrint(self):
+        if self.ok:
+            return "ok %s - %s" % (self.id, self.desc)
+        else:
+            return "not ok %s - %s" % (self.id, self.desc)
+
+    def printTests(self):
+        tabs=""
+        for x in range(self.nivel-1):
+            tabs += "\t"
+        print("%s%s"% (tabs,self.getPrint()))
+        if self.subtests:
+            self.printSTests()
+
+    def printSTests(self):
+        for test_ in self.subtests:
+            test_.printTests()
+
+
+
 
