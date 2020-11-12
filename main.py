7f677f66e9b885eb99a7ec577d760e44a333ad43
Miguel Silva <a18829@alunos.ipca.pt>
2020-11-12 20:27:36 +0000

    v4 IT WORKS BITCHES

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-#import re
+# import re
+import time
+from itertools import tee
+
 import ply.lex as lex
 from utils import readFile, Test, getNivel
 
 tests = []
+tests_aux = []
+
 
 # t1 = Test(1, True, "test 1", 1)
 # t2 = Test(2, True, "test sub1", 2)
 # t3 = Test(3, True, "test sub sub1", 3)
+#
+# tests_aux.append(t1)
+# tests_aux.append(t2)
+# tests_aux.append(t3)
+
+
 # t4 = Test(4, True, "test 2", 1)
 # t2.subtests.append(t3)
 # t1.subtests.append(t2)
@@ -16,46 +27,91 @@ tests = []
 # for t in tests:
 #     t.printTests()
 
+def auxOrg(test, listX):
+    level = test.nivel
+    if listX == None:
+        listX.append(test)
+    else:
+        nivel = listX[-1].nivel
+        if nivel < level:
+            listX.append(test)
+        elif nivel == level:
+            if nivel == 1:
+                aux = listX[-1]
+                listX[-1] = test
+                return aux
+            else:
+                listX[-2].subtests.append(listX[-1])
+                aux = listX[-1]
+                del listX[-1]
+                listX.append(test)
+                return aux
+        else:
+            while nivel != level:
+                listX[-2].subtests.append(listX[-1])
+                del listX[-1]
+                nivel = listX[-1].nivel
+            if nivel == 1:
+                aux = listX[-1]
+                del listX[-1]
+                listX.append(test)
+                return aux
+            else:
+                listX[-2].subtests.append(listX[-1])
+                aux = listX[-1]
+                del listX[-1]
+                listX.append(test)
+                return aux
+
+
 tokens = ("OK", "NOTOK", "SUBOK", "SUBNOTOK", "TAB", "COMMENT", "COUNTER")
+
 
 def t_COUNTER(t):
     r"\d.+.\n"
     return t
 
+
 def t_OK(t):
-    r"^ok.+\n"
+    r"ok.+\n"
     return t
+
 
 def t_SUBOK(t):
     r"(\s{4})+ok.+\n"
     return t
 
+
 def t_NOTOK(t):
     r"^not\sok.+\n"
     return t
+
 
 def t_SUBNOTOK(t):
     r"(\s{4})+not\sok.+\n"
     return t
 
+
 def t_TAB(t):
     r"\ {4}"
     pass
+
 
 def t_COMMENT(t):
     r"\n|[ \t].*\n|.+\n"
     return t
 
+
 def t_error(t):
     print("Unknown token: [%s]" % t.value)
     exit(1)
 
-pointer_record = []
+
 lexer = lex.lex()
-lexer.input(readFile("test/teste3.t"))
+lexer.input(readFile("test/testedummy.t"))
 
 for token in iter(lexer.token, None):
-    pass
+    taux = None
     if token.type == "SUBOK":
         ws = getNivel(token.value)
         saux = token.value.strip()
@@ -63,7 +119,25 @@ for token in iter(lexer.token, None):
     if token.type == "OK":
         taux = Test(int(token.value[3]), True, token.value[6:], 1)
     if token.type == "NOTOK":
-        taux = Test(int(token.value[3]), False, token.value[6:], 1)
+        taux = Test(int(token.value[7]), False, token.value[11:], 1)
+    if taux:
 
-# for test_ in tests:
-#     test_.printTests()
+        nivel = taux.nivel
+        if tests_aux:
+            if nivel == 1:
+                a = auxOrg(taux, tests_aux)
+                if a:
+                    tests.append(a)
+            else:
+                auxOrg(taux, tests_aux)
+        else:
+            tests_aux.append(taux)
+
+while tests_aux[-1].nivel != 1:
+    tests_aux[-2].subtests.append(tests_aux[-1])
+    del tests_aux[-1]
+
+tests.append(tests_aux[0])
+
+for test_ in tests:
+    test_.printTests()
---
+++ test/testedummy.t
@@ -0,0 +1,25 @@
+1..3
+ok 1
+ok 2
+# Subtest: Some subtests first
+    ok 1 - my subtest 1
+    # Subtest: Some subtests second
+        ok 1 - my other subtest 1
+        # Subtest: Some subtests third
+            ok 1 - my test bla bla bla
+        ok 2 - and another subtest 2
+        1..2
+    ok 2 - Some subtests second
+    ok 3 - some more subtests
+    1..3
+ok 3 - Some subtests first
+# Subtest: Some subtests first
+    ok 1 - 123455
+    ok 2 - 123455
+    ok 3 - 123455
+    # Subtest: Some subtests second
+        ok 1 - MABS
+        # Subtest: Some subtests third
+            ok 1 - MABS
+            # Subtest: Some subtests forth
+                ok 1 - MABS
